eae99263c3fb21116a738753af5301bf2ce636ca
ujigawamao <ujigawamao@users.noreply.github.com>
2021-10-27 04:24:46 +0000

    課題２を追加

---
+++ kadai-02/index.js
@@ -0,0 +1,13 @@
+const firstname = "mami"
+const familyname = "takeuchi"
+console.log(firstname + familyname)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const dog = false
+const cat = false
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
+
